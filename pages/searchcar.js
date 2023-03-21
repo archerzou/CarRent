1dca4544994677f4353856c9923ee1fc024d2423
tidbitsjs <tidbitsjs@gmail.com>
2023-03-21 17:02:52 +0530

    updated code using axios

--- pages/searchcar.js
+++ pages/searchcar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 import Header from '../components/header';
 import Footer from '../components/footer';
@@ -15,13 +16,32 @@ const SearchCar = ({cars}) => {
     </>
 )}
 
-export async function getServerSideProps() {
-  // Fetch data from external API
-  const res = await fetch('http://localhost:3000/api/cars');
-  const cars = await res.json()
+// export async function getServerSideProps() {
+//   // Fetch data from external API
+//   const res = await fetch('http://localhost:3000/api/cars');
+//   const cars = await res.json()
 
-  // Pass data to the page via props
-  return { props: { cars } }
+//   // Pass data to the page via props
+//   return { props: { cars } }
+// }
+
+// Alternative method using Axios
+
+export async function getServerSideProps() {
+  try {
+    const response = await axios.get('http://localhost:3000/api/cars');
+    const cars = response.data;
+
+    return {
+      props: { cars },
+    };
+  } catch (error) {
+    console.log(error.message);
+    return {
+      props: { cars: [] },
+    };
+  }
 }
+
 
 export default SearchCar;
