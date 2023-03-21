40edc58e20f727a26c69631abdef2b757abc2bfb
tidbitsjs <tidbitsjs@gmail.com>
2023-03-21 16:56:36 +0530

    fix server side rendering

--- pages/searchcar.js
+++ pages/searchcar.js
@@ -4,13 +4,24 @@ import Header from '../components/header';
 import Footer from '../components/footer';
 import Search from '../components/Search';
 
-const SearchCar = () => (
-  <>
-    <Header />
-    <Search />
-    <Footer />
-  </>
+const SearchCar = ({cars}) => {
+  console.log(cars)
+  
+  return(
+    <>
+      <Header />
+      <Search />
+      <Footer />
+    </>
+)}
 
-);
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch('http://localhost:3000/api/cars');
+  const cars = await res.json()
+
+  // Pass data to the page via props
+  return { props: { cars } }
+}
 
 export default SearchCar;
